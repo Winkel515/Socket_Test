af4c160978faee6502e215cd83d492066c4ff8db
Winkel515 <winkel515@hotmail.com>
2020-05-02 17:49:18 -0400

    Changed port to 80

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-const port = 3000;
+const port = 80;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
